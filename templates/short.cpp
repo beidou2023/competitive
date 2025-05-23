eaf54f6ef445c7984ed4c0e46dcd589482c03555
beidou2023 <131274568+beidou2023@users.noreply.github.com>
2025-05-22 21:15:24 -0400

    template corto

---
+++ templates/short.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int         ll
+using   namespace   std;
+typedef long long   ll;
+
+//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+void solve(){
+
+}
+
+signed main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int tt=1;
+    // cin>>tt;
+    while(tt--){
+        solve();
+    }
+    return 0;
+}
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
